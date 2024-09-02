2da2ded50b527ba18fc30419c846bd7282cbac13
Darshan Parmar <67963288+darshankparmar@users.noreply.github.com>
2024-09-02 22:01:20 +0530

    Update Header.js

--- data-structure-visualization/src/MyComponents/Header/Header.js
+++ data-structure-visualization/src/MyComponents/Header/Header.js
@@ -10,13 +10,15 @@ export default function Header() {
                 <div className="container-fluid all-show"> <Link className="navbar-brand" to='/'>VisualDSA <i className="fas fa-code-branch"></i></Link> <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span> </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
-                            {/*   */}
+                            {   }
                             {/* <li className="nav-item"> <Link className="nav-link" to='/'><i className="fa fa-search"></i></Link> </li> */}
-                            {/* <li className="nav-item"> <a className="nav-link" href='https://github.com/darshankparmar' rel="noreferrer" target="_blank"><i className="fab fa-3x fa-github"></i></a> </li> */}
+                            <li className="nav-item"> <a className="nav-link" href='https://github.com/darshankparmar' rel="noreferrer" target="_blank"><i className="fab fa-3x fa-github"></i></a> </li>
                         </ul> 
                         <div className="d-flex flex-column sim">                             
                             <div className="media d-flex align-items-center">
-                                <img src="https://bootstrapious.com/i/snippets/sn-v-nav/avatar.png" alt="..." width="65" className="mr-3 rounded-circle img-thumbnail shadow-sm" />
+                                <div className="mr-3 rounded-circle img-thumbnail shadow-sm">
+                                    <i className="fa-solid fa-user"></i>
+                                </div>
                                 <div className="media-body">
                                     <h5 className="m-0">username</h5>   
                                 </div>
